98608d93336e2c4bff4b7b409a1bfa804fdda677
sahil-soni18 <100425303+sahil-soni18@users.noreply.github.com>
2024-12-20 17:43:42 +0530

    Java Arrays

--- Syntax.java
+++ Syntax.java
@@ -42,6 +42,42 @@ public class Syntax {
             System.out.println(i);
         }
 
+        // Array
+
+        // Ways Of Declaring an Array In Java.
+
+        // Way 1
+
+        int[] numbersArray = new int[5];
+
+        System.out.println(numbersArray);    // Prints the default toString() representation of the array object in Java.: [I@72a7c7e0
+
+        numbersArray[0] = 2;
+        numbersArray[1] = 18;
+        numbersArray[2] = 28;
+        numbersArray[3] = 1;
+        numbersArray[3] = 3;
+        // numbersArray[4] = 31;
+        // numbersArray[5] = 100;    // Out of bounds.
+
+        for ( int i = 0; i < numbersArray.length; i++ ) {
+            System.out.println(numbersArray[i]);
+        }
+
+        // By default, all elements of an int array in Java are initialized to 0.
+
+        // Way 2.
+
+        int[] numbersArray2 = { 5, 2, 3, 18, 28 };
+
+        System.out.println(numbersArray2.length);
+
+        for ( int i = 0; i < numbersArray2.length; i++ ) {
+            System.out.println(numbersArray2[i]);
+        }
+
+        // numbersArray2[5] = 100;   // Testing if this approach can expand the size of array.   Gives Out of bound error.
+
         scanner.close();   // Closing the scanner object.
     }
 }
